12a0d91f08b1f501ec249594b80200cda1bcf4b3
Vladimir <jul-pro@ukr.net>
2018-01-15 18:47:28 +0200

    Локализация

--- functions.php
+++ functions.php
@@ -1,14 +1,24 @@
 <?php
 
-function mfwthemeprojectchild_scripts() {
-    wp_enqueue_style( 'mfwthemeprojectchild-style', get_stylesheet_uri(), array( 'mfwtp-main' ) );
+define('T_TD', 'mfwthemeprojectchild');
+
+function mfwthemeprojectchild_setup() {
+    load_child_theme_textdomain( T_TD, get_stylesheet_directory() . '/languages/' );
+    
 }
+add_action('after_setup_theme', 'mfwthemeprojectchild_setup');
+
+
+function mfwthemeprojectchild_scripts() {
+    wp_enqueue_style( 'mfwthemeprojectchild-style', get_stylesheet_uri() );
+}
+add_action('wp_enqueue_scripts', 'mfwthemeprojectchild_scripts');
 
 function filter_title($title) {
     if( isset( $title['site'] ) ) {
         $title['site'] .= ' - MFW Theme Project Child Theme';
     } else {
-        $title['site'] .= 'MFW Theme Project Child Theme';
+        $title['site'] = 'MFW Theme Project Child Theme';
     }
     
     return $title;
@@ -21,3 +31,19 @@ function filter_content($content) {
 }
 
 add_filter('the_content', 'filter_content');
+
+function add_theme_menu() {
+    add_theme_page(
+        __('Page title', T_TD), 
+        __('Menu title', T_TD),
+        'read',
+        'mfwthemeprojectchild_menu_page', 
+        'render_child_theme_page'
+    );
+}
+
+function render_child_theme_page() {
+    _e('Child theme page.');
+}
+
+add_action('admin_menu', 'add_theme_menu');
